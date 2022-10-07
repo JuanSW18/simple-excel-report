1971575709b611e650d9a858917507e5cb0cd210
Juan Eneque <juan.eneque@gestionysistemas.com>
2022-10-06 22:57:37 -0500

    listar pokemones en tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPokemonInfo } from './shared/interfaces/pokemon-response.interface';
+import { PokemonService } from './shared/services/pokemon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simple-excel-report';
+export class AppComponent implements OnInit {
+  title = 'Simple Excel Report';
+  pokemonList: IPokemonInfo[] = [];
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.getPokemonList();
+  }
+
+  getPokemonList() {
+    this.pokemonService.getPokemonList(0, 10).subscribe({
+      next: (response) => {
+        this.pokemonList = [...response.results];
+      },
+    })
+  }
 }
